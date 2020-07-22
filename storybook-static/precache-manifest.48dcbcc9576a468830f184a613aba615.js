be1356f4a69d886a7eab7e08d1564eeab876314f
papa31 <visidoor@yandex.ru>
2020-07-22 19:41:45 +0500

    Build storybook

---
+++ storybook-static/precache-manifest.48dcbcc9576a468830f184a613aba615.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d62f9d0a6866d6e27bbd2ee3d4c84fd7",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.df36b39d9edf007ed1b0.bundle.js"
+  },
+  {
+    "url": "runtime~main.df36b39d9edf007ed1b0.bundle.js"
+  },
+  {
+    "revision": "8ff7d4d77a409561f31403added8a029",
+    "url": "static/media/SignIn.8ff7d4d7.svg"
+  },
+  {
+    "revision": "d65ff19649668571877226aa78d2abf1",
+    "url": "static/media/SignUp.d65ff196.svg"
+  },
+  {
+    "url": "vendors~main.df36b39d9edf007ed1b0.bundle.js"
+  },
+  {
+    "url": "vendors~main.df36b39d9edf007ed1b0.bundle.js.LICENSE.txt"
+  }
+]);
