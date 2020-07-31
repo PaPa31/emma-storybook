b015e3c621544001a43b3a7aa05880c68604dcab
PaPa31 <PaPa31@users.noreply.github.com>
2020-07-31 13:51:39 +0000

    deploy: dfa365219d81880cef23600d2d58bcb364a16cfb

---
+++ precache-manifest.13ce344bf46208f034b183c89a3fa58e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8beb00d9cb3e913c1a15a430a07a322",
+    "url": "/emma-storybook/index.html"
+  },
+  {
+    "revision": "28bcac45ca38f769f150",
+    "url": "/emma-storybook/static/js/2.34bd55f5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/emma-storybook/static/js/2.34bd55f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e16acba77075dbe10642",
+    "url": "/emma-storybook/static/js/main.4f5a4a22.chunk.js"
+  },
+  {
+    "revision": "e9d6819539ffa1153347",
+    "url": "/emma-storybook/static/js/runtime-main.9862f0a0.js"
+  },
+  {
+    "revision": "8ff7d4d77a409561f31403added8a029",
+    "url": "/emma-storybook/static/media/SignIn.8ff7d4d7.svg"
+  },
+  {
+    "revision": "d65ff19649668571877226aa78d2abf1",
+    "url": "/emma-storybook/static/media/SignUp.d65ff196.svg"
+  }
+]);
